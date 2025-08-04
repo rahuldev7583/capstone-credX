7b4ec86aa36385342eebd9bab4ee5c5944361fd7
rahuldev7583 <rahuldev7583@gmail.com>
2025-08-04 09:10:23 +0530

    initial commit

---
+++ programs/cred-x/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("Ces2ZsycAiQy79EKb9JPcCVosr3FzvrzWEpEy9XRZif5");
+
+#[program]
+pub mod cred_x {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
